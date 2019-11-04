cb23747f775550aec9afe10b36120e93d2055a87
ERROR404 <32724548+hebtuerror404@users.noreply.github.com>
2019-11-04 17:35:46 +0800

    Add New

---
+++ 2019_HECTF/Reverse/math/math.py
@@ -0,0 +1,20 @@
+import sys
+a = -80538738812075974
+b = 80435758145817515 # 0x 11dc3e557854bab
+c = 12602123297335631 # 0x 2cc5907a8da94f
+
+a = int(input(a = ))
+
+b_ = str(input())
+b = int(b_, 16)
+
+c_ = str(input())
+c = int(c_, 16)
+
+assert(a  0)
+
+if b = c
+    b, c = c, b
+
+if aaa+bbb+ccc==42
+    print(You win!flag is MD5(+str(a)+str(b)+str(c)+))
